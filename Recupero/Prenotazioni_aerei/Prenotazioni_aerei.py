9e5ff3820ba6fb97e51f38608ad3135cbaa72e6d
nataliiaborovyk <nataliia.borovyk@gmail.com>
2025-07-02 22:58:54 +0200

    update 02.07

--- Recupero/Prenotazioni_aerei/Prenotazioni_aerei.py
+++ Recupero/Prenotazioni_aerei/Prenotazioni_aerei.py
@@ -51,6 +51,7 @@ class VoloCommerciale(Volo):
         return self._posti_disp
     
     def prenota_posto(self, posti: int, classe_servizio:str):
+        self.posti_disponibili()
         if classe_servizio not in self._posti_disp:
             print(f"La classe {classe_servizio} non è presente nel volo {self._codice_volo}") 
         # for k, v in self._posti_disp.items():
@@ -70,7 +71,7 @@ class VoloCommerciale(Volo):
 
                 print(f"Hai prenotato {posti} in classe {classe_servizio} di volo{self._codice_volo}")
             else:
-                print(f"Purtroppo non ci sono {posti}  disponibili in classe {classe_servizio} di volo{self._codice_volo}")
+                print(f"Purtroppo non ci sono {posti} posti disponibili in {classe_servizio} di volo {self._codice_volo}")
         else:
             print(f"Il volo {self._codice_volo} è completo")
 
@@ -81,9 +82,84 @@ class VoloCommerciale(Volo):
                 \nClass Business: max posti: {self._posti_business}, prenotati: {self._prenotazioni_business}, disponibili: {self._posti_disp['Classe business']} \
                 \nClass Prima: max posti: {self._posti_prima}, prenotati: {self._prenotazioni_prima}, disponibili: {self._posti_disp['Prima classe']} "
 
+class VoloCharter(Volo):
+    _costo_volo: float
+    _posti_prenotati: int
+    _posti_disponibili: int
+
+    def __init__(self, codice_volo:str, max_posti:int, costo_volo:float) -> None:
+        super().__init__(codice_volo, max_posti)
+        self._costo_volo = costo_volo
+        self._posti_prenotati = 0
+        self._posti_disponibili = self._max_posti
+        self._prenotato = False
+
+    def prenota_posto(self):
+        if self._posti_disponibili == self._max_posti:
+            self._posti_disponibili = 0
+            self._posti_prenotati = self._max_posti
+            self._prenotazioni = self._max_posti
+            print(f"Il volo {self._codice_volo} è stato prenotato completamente")
+        else:
+            print(f"Purtroppo il volo {self._codice_volo} è gia prenotato")
+
+    def posti_disponibili(self):
+        return self._posti_disponibili
+    
+class CompagniaAerea:
+    _nome: str
+    _prezzo_standart: float
+    _flotta: list[Volo]
+
+    def __init__(self, nome:str, prezzo_standart:float) -> None:
+        self._nome = nome
+        self._prezzo_standart = prezzo_standart
+        self._flotta = []
+
+    def aggiungi_volo(self, volo:VoloCommerciale) -> None:
+        if volo in self._flotta:
+            raise ValueError("Errore, il volo è gia presente")
+        self._flotta.append(volo)
+
+    def rimuovi_volo(self, volo:VoloCommerciale) -> None:
+        if volo not in self._flotta:
+            raise ValueError("Errore, il volo non è presente")
+        self._flotta.remove(volo)
+
+    def mostra_flotta(self) -> str:
+        return [i._codice_volo for i in self._flotta]
+    
+    def guadagno(self) -> float:
+        guadagno: float = 0
+        for i in self._flotta:
+            if type(i) == VoloCommerciale:
+                guadagno += ((i._prenotazioni_economica * self._prezzo_standart) 
+                             + (i._prenotazioni_business * self._prezzo_standart * 2)
+                             + (i._prenotazioni_prima * self._prezzo_standart * 3))
+            elif type(i) == VoloCharter:
+                guadagno += i._costo_volo
+        return round(guadagno, 2)
+
+
 if __name__ == "__main__":
 
     v1:VoloCommerciale = VoloCommerciale("AAA33", 150)
     print(v1)
     v1.prenota_posto(50, "Classe economica")
     print(v1)
+    v1.prenota_posto(15, "Prima classe")
+    print(v1)
+    v1.prenota_posto(5, "Prima classe")
+    print(v1)
+
+    v2:VoloCharter = VoloCharter("BBB44", 100, 1000)
+    print(v2.posti_disponibili())
+    v2.prenota_posto()
+    print(v2.posti_disponibili())
+    v2.prenota_posto()
+
+    ca: CompagniaAerea = CompagniaAerea("WizAir", 10)
+    ca.aggiungi_volo(v1)
+    ca.aggiungi_volo(v2)
+    print(ca.guadagno())
+    print(ca.mostra_flotta())
